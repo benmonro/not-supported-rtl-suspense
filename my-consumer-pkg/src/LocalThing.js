69dcfe236b72f04674de4a1a0a625c1ca1bec479
Ben Monro <ben.monro@pwc.com>
2019-03-10 19:01:27 -0700

    added local thing test

---
+++ my-consumer-pkg/src/LocalThing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LocalThing = <div>my local thing</div>;
+
+export default LocalThing;
--- my-consumer-pkg/tests/index.test.js
+++ my-consumer-pkg/tests/index.test.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 import { render, waitForElement, cleanup } from "react-testing-library";
-import MyPackageThing from "my-package/lib/my-thing";
+import MyOtherPackageThing from "my-package/lib/my-thing";
 import LazyThing from "../src/index";
 
 afterEach(cleanup);
 
 test("it works", async () => {
-  const { getByText, debug } = render(<MyPackageThing />);
+  const { getByText, debug } = render(<MyOtherPackageThing />);
 
   await waitForElement(() => getByText("my thing"));
 
   expect(getByText("my thing"));
 });
 
-test("it says not suported, like wtf", async () => {
+test("it lazy loads a local component", async () => {
+  const LazyLocalThing = React.lazy(() => import("../src/LocalThing"));
+  const { getByText, debug } = render(
+    <React.Suspense fallback="Loading...">
+      <LazyLocalThing />
+    </React.Suspense>
+  );
+  debug();
+  await waitForElement(() => getByText("my local thing"));
+  debug();
+  expect(getByText("my local thing"));
+});
+
+test("it says not supported, like wtf", async () => {
   const { getByText, debug } = render(<LazyThing />);
   debug();
   await waitForElement(() => getByText("my thing"));
