27207b0aabbdf082e9a40a9d8b1dd0546b7f8ef3
Ben Monro <ben.monro@pwc.com>
2019-03-10 19:11:35 -0700

    fixed path in app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { LazyThing } from "my-consumer-pkg";
+import LazyThing from "my-consumer-pkg";
 
 class App extends Component {
   render() {
--- my-consumer-pkg/lib/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export function LazyThing() {
-  var MyLazyThing = React.lazy(function () {
-    return import("my-package/lib/my-thing");
-  });
-  return React.createElement(React.Suspense, {
-    fallback: "Loading..."
-  }, React.createElement(MyLazyThing, null));
-}
